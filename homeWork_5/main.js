64e48274e8fce0e8856598147bbb7aed6c81c826
My Name <kostya-novikov@mail.ru>
2020-01-31 17:28:45 +0300

    work

--- homeWork_5/main.js
+++ homeWork_5/main.js
@@ -1,24 +1,51 @@
-let square;
+let square,main;
+
+
+
+
 
 function include(){
   for(i=1; i<=8; i++){
-    let square = document.createElement("div"+i);
-    document.body.append(square);
+    let main = document.querySelector("#container");
+    let square = document.createElement("div");
+    main.appendChild(square);
 
-    document.querySelector('div'+i).style.width = '100px';
-    document.querySelector('div'+i).style.height = '100px';
+    square.className= "block number_" +i;
+    square.id = "someID_"+i;
 
       if(i%2!=0){
-        document.querySelector('div'+i).style.background = 'black';
+        square.classList.add('black');
       }
       else{
-        document.querySelector('div'+i).style.background = 'white';
+        square.classList.add('white');
       }
-  }
+  } 
+}
+
+function double(){
+  
+  for(i=1; i<=8; i++){
+    let parent = document.querySelector("#someID_"+i);
+
+    for(i=1; i<=8; i++) {
+      let two = document.createElement("div");
+      parent.appendChild(two);
+   
+      two.className= "horizont" ;
+        
+        if(i%2!=0){
+          two.classList.add('black');
+        }
+        else{
+          two.classList.add('white');
+        }
+    }
+   
+  } 
 }
 
 include();
-
+double();
 
 
 
